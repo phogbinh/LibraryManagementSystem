c0761a8d66b060fcba5a69cb99edbd8db9da9ff5
kyo <phongbinh2511@gmail.com>
2020-03-19 17:15:57 +0800

    [feature-implement-homework-2] Added implementations and tests for the class `Library`.

--- src/main/java/org/ntutssl/library/Library.java
+++ src/main/java/org/ntutssl/library/Library.java
@@ -4,9 +4,24 @@ import java.util.ArrayList;
 
 public class Library 
 {
-    public Library();
+    ArrayList< Item > _items;
 
-    public void add( Item item );
+    public Library()
+    {
+        _items = new ArrayList< Item >();
+    }
 
-    public Item getItem( int index );
+    public void add( Item item )
+    {
+        _items.add( item );
+    }
+
+    public Item getItem( int index )
+    {
+        if ( index < 0 || _items.size() <= index )
+        {
+            throw new IllegalArgumentException( Definitions.ERROR_INDEX_IS_OUT_OF_RANGE );
+        }
+        return _items.get( index );
+    }
 }
