cd42ab944a24f9b89e6fc869b0b72164afaae3ba
kyo <phongbinh2511@gmail.com>
2020-03-19 16:37:07 +0800

    [feature-implement-homework-2] Formatted the interface `Item`.

--- src/main/java/org/ntutssl/library/Item.java
+++ src/main/java/org/ntutssl/library/Item.java
@@ -3,10 +3,10 @@ package org.ntutssl.library;
 
 public interface Item 
 {
-    public void add(Item item);
+    public void add( Item item );
     public String name();
     public String description();
     public String isbn();
     public String author();
-    public Item getItem(int index);
+    public Item getItem( int index );
 }
