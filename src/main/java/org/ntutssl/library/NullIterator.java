acabb9bc0caf47784c7eb1f1e9ce01fcdd6b24d4
kyo <phongbinh2511@gmail.com>
2020-03-26 19:13:32 +0800

    [feature-implement-homework-3] Formatted `NullIterator.java`.

--- src/main/java/org/ntutssl/library/NullIterator.java
+++ src/main/java/org/ntutssl/library/NullIterator.java
@@ -3,7 +3,7 @@ package org.ntutssl.library;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class NullIterator implements Iterator<Item>
+public class NullIterator implements Iterator< Item >
 {
     private final String ERROR_NEXT_IS_CALLED_ON_A_NULL_ITERATOR_OBJECT = "The function `next()` is called on a null iterator object";
 
