df2895af49bbc95988e1fe7cc626b9a20196999a
kyo <phongbinh2511@gmail.com>
2020-03-26 16:04:52 +0800

    [feature-add-homework-3-skeleton] Removed a now redundant constant due to the removal of `Readable.getItem( int )`.

--- src/main/java/org/ntutssl/library/Readable.java
+++ src/main/java/org/ntutssl/library/Readable.java
@@ -5,7 +5,6 @@ public abstract class Readable implements Item
     private final String ERROR_ADD_IS_CALLED_ON_A_BOOK_OBJECT = "The function `add()` is called on a book object";
     private final String ERROR_ISBN_IS_CALLED_ON_A_COLLECTION_OBJECT = "The function `isbn()` is called on a collection object";
     private final String ERROR_AUTHOR_IS_CALLED_ON_A_COLLECTION_OBJECT = "The function `author()` is called on a collection object";
-    private final String ERROR_GET_ITEM_IS_CALLED_ON_A_BOOK_OBJECT = "The function `getItem( int )` is called on a book object";
 
     @Override
     public void add( Item item )
