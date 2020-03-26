ab227634c4f517bb3567992e61950e788ed8088d
kyo <phongbinh2511@gmail.com>
2020-03-26 19:10:19 +0800

    [feature-implement-homework-3] Added implementations and tests for methods of `NullIterator`.

--- src/main/java/org/ntutssl/library/NullIterator.java
+++ src/main/java/org/ntutssl/library/NullIterator.java
@@ -1,10 +1,21 @@
 package org.ntutssl.library;
 
-public class NullIterator implements Iterator< Item >
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class NullIterator implements Iterator<Item>
 {
-    @Override
-    public boolean hasNext();
+    private final String ERROR_NEXT_IS_CALLED_ON_A_NULL_ITERATOR_OBJECT = "The function `next()` is called on a null iterator object";
 
     @Override
-    public Item next();
+    public boolean hasNext()
+    {
+        return false;
+    }
+
+    @Override
+    public Item next()
+    {
+        throw new NoSuchElementException( ERROR_NEXT_IS_CALLED_ON_A_NULL_ITERATOR_OBJECT );
+    }
 }
--- src/test/java/org/ntutssl/library/NullIteratorTest.java
+++ src/test/java/org/ntutssl/library/NullIteratorTest.java
@@ -1,5 +1,38 @@
 package org.ntutssl.library;
 
+import static org.junit.Assert.assertFalse;
+
+import java.util.NoSuchElementException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 public class NullIteratorTest 
-{  
+{
+    private NullIterator _nullIterator;
+
+    @Before
+    public void setUp()
+    {
+        _nullIterator = new NullIterator();
+    }
+
+    @After
+    public void tearDown()
+    {
+        /* Body intentionally empty */
+    }
+
+    @Test
+    public void test_has_next_of_null_iterator_being_false()
+    {
+        assertFalse( _nullIterator.hasNext() );
+    }
+
+    @Test( expected = NoSuchElementException.class )
+    public void test_next_being_called_on_null_iterator_throwing_exception()
+    {
+        _nullIterator.next();
+    }
 }
