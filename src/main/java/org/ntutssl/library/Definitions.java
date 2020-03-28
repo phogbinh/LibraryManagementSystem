e5baeb0c90b000e920b481e5f748edb5f1be324e
kyo <phongbinh2511@gmail.com>
2020-03-28 16:12:35 +0800

    [feature-implement-homework-3] Added implementations for `Main` and `InputOutput`.

---
+++ src/main/java/org/ntutssl/library/Definitions.java
@@ -0,0 +1,15 @@
+package org.ntutssl.library;
+
+public final class Definitions
+{
+    public static String COLON = ":";
+    public static String EMPTY = "";
+    public static String END_LINE = "\n";
+    public static String INDENT = "\t";
+    public static String SPACE = " ";
+
+    private Definitions()
+    {
+        /* Body intentionally empty */
+    }
+}
