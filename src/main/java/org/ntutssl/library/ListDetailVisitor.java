00aee6efaea577da1d20bf1b28933fbd4a334a5a
kyo <phongbinh2511@gmail.com>
2020-04-04 22:45:29 +0800

    [hotfix-list-detail-visitor] Overhauled `ListDetailVisitor` so that it lists children of a collection by one indent from the collection.

--- src/main/java/org/ntutssl/library/ListDetailVisitor.java
+++ src/main/java/org/ntutssl/library/ListDetailVisitor.java
@@ -1,5 +1,7 @@
 package org.ntutssl.library;
 
+import java.util.Iterator;
+
 public class ListDetailVisitor implements Visitor 
 {
     public static String BOOK_NAME = "Book Name" + Definitions.COLON + Definitions.SPACE;
@@ -9,26 +11,50 @@ public class ListDetailVisitor implements Visitor
     public static String COLLECTION_NAME = "Collection Name" + Definitions.COLON + Definitions.SPACE;
     public static String COLLECTION_DESCRIPTION = "Description" + Definitions.COLON + Definitions.SPACE;
     private String _itemInfo;
+    private int _level;
 
     public ListDetailVisitor()
     {
-        /* Body intentionally empty */
+        _itemInfo = "";
+        _level = 0;
     }
 
     @Override
     public void visitBook( Book book )
     {
-        _itemInfo = BOOK_NAME + book.name() + Definitions.END_LINE
-            + Definitions.INDENT + BOOK_AUTHOR + book.author() + Definitions.END_LINE
-            + Definitions.INDENT + BOOK_DESCRIPTION + book.description() + Definitions.END_LINE
-            + Definitions.INDENT + BOOK_ISBN + book.isbn() + Definitions.END_LINE;
+        indentItemInfo( 1 );
+        _itemInfo += BOOK_NAME + book.name() + Definitions.END_LINE;
+        indentItemInfo( 0 );
+        _itemInfo += BOOK_AUTHOR + book.author() + Definitions.END_LINE;
+        indentItemInfo( 0 );
+        _itemInfo += BOOK_DESCRIPTION + book.description() + Definitions.END_LINE;
+        indentItemInfo( 0 );
+        _itemInfo += BOOK_ISBN + book.isbn() + Definitions.END_LINE;
+        _level--;
     }
 
     @Override
     public void visitCollection( Collection collection )
     {
-        _itemInfo = COLLECTION_NAME + collection.name() + Definitions.END_LINE
-            + Definitions.INDENT + COLLECTION_DESCRIPTION + collection.description() + Definitions.END_LINE;
+        indentItemInfo( 1 );
+        _itemInfo += COLLECTION_NAME + collection.name() + Definitions.END_LINE;
+        indentItemInfo( 0 );
+        _itemInfo += COLLECTION_DESCRIPTION + collection.description() + Definitions.END_LINE;
+        Iterator< Item > iterator = collection.iterator();
+        _level++;
+        while ( iterator.hasNext() )
+        {
+            iterator.next().accept( this );
+        }
+        _level--;
+    }
+
+    private void indentItemInfo( int startingIndentsCount )
+    {
+        for ( int indentsCount = startingIndentsCount; indentsCount <= _level; indentsCount++ )
+        {
+            _itemInfo += Definitions.INDENT;
+        }
     }
 
     @Override
