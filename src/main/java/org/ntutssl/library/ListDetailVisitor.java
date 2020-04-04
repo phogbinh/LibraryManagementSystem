43255f288535eb8e73ae44dca78e354fb4ded332
kyo <phongbinh2511@gmail.com>
2020-04-04 21:37:21 +0800

    [feature-implement-homework-4] Added implementations and tests for the class `ListDetailVisitor`.

--- src/main/java/org/ntutssl/library/ListDetailVisitor.java
+++ src/main/java/org/ntutssl/library/ListDetailVisitor.java
@@ -2,14 +2,38 @@ package org.ntutssl.library;
 
 public class ListDetailVisitor implements Visitor 
 {
-    public ListDetailVisitor();
+    public static String BOOK_NAME = "Book Name" + Definitions.COLON + Definitions.SPACE;
+    public static String BOOK_AUTHOR = Definitions.INDENT + "Author" + Definitions.COLON + Definitions.SPACE;
+    public static String BOOK_DESCRIPTION = Definitions.INDENT + "Description" + Definitions.COLON + Definitions.SPACE;
+    public static String BOOK_ISBN = Definitions.INDENT + "ISBN" + Definitions.COLON + Definitions.SPACE;
+    public static String COLLECTION_NAME = "Collection Name" + Definitions.COLON + Definitions.SPACE;
+    public static String COLLECTION_DESCRIPTION = Definitions.INDENT + "Description" + Definitions.COLON + Definitions.SPACE;
+    private String _itemInfo;
+
+    public ListDetailVisitor()
+    {
+        /* Body intentionally empty */
+    }
 
     @Override
-    public void visitBook( Book book );
+    public void visitBook( Book book )
+    {
+        _itemInfo = BOOK_NAME + book.name() + Definitions.END_LINE
+            + Definitions.INDENT + BOOK_AUTHOR + book.author() + Definitions.END_LINE
+            + Definitions.INDENT + BOOK_DESCRIPTION + book.description() + Definitions.END_LINE
+            + Definitions.INDENT + BOOK_ISBN + book.isbn() + Definitions.END_LINE;
+    }
 
     @Override
-    public void visitCollection( Collection collection );
+    public void visitCollection( Collection collection )
+    {
+        _itemInfo = COLLECTION_NAME + collection.name() + Definitions.END_LINE
+            + Definitions.INDENT + COLLECTION_DESCRIPTION + collection.description() + Definitions.END_LINE;
+    }
 
     @Override
-    public String getResult();
+    public String getResult()
+    {
+        return _itemInfo;
+    }
 }
--- src/test/java/org/ntutssl/library/ListDetailVisitorTest.java
+++ src/test/java/org/ntutssl/library/ListDetailVisitorTest.java
@@ -1,6 +1,86 @@
 package org.ntutssl.library;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import java.lang.reflect.Field;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 public class ListDetailVisitorTest 
 {
-    
+    private final String ITEM_INFO_CANNOT_BE_GOT = "The member variable item info cannot be got";
+    private final String MEMBER_VARIABLE_NAME_ITEM_INFO = "_itemInfo";
+    private Visitor _visitor;
+
+    private String GetItemInfo()
+    {
+        try
+        {
+            Field itemInfoField = ListDetailVisitor.class.getDeclaredField( MEMBER_VARIABLE_NAME_ITEM_INFO );
+            itemInfoField.setAccessible( true );
+            try
+            {
+                return ( String )itemInfoField.get( _visitor );
+            }
+            catch( IllegalAccessException exception )
+            {
+                assertTrue( false );
+            }
+        }
+        catch( NoSuchFieldException exception )
+        {
+            assertTrue( false );
+        }
+        throw new IllegalStateException( ITEM_INFO_CANNOT_BE_GOT );
+    }
+
+    @Before
+    public void setUp()
+    {
+        _visitor = new ListDetailVisitor();
+    }
+
+    @After
+    public void tearDown()
+    {
+        /* Body intentionally empty */
+    }
+
+    @Test
+    public void test_visiting_book_setting_item_info_to_book_info()
+    {
+        final String BOOK_NAME = "Introduction to Algorithms, Third Edition";
+        final String BOOK_DESCRIPTION = "The book covers a broad range of algorithms in depth, yet makes their design and analysis accessible to all levels of readers.";
+        final String BOOK_AUTHOR = "Thomas H. Cormen";
+        final String BOOK_ISBN = "9780262033848";
+        final String EXPECTED_STRING = ItemHelper.BOOK_NAME + BOOK_NAME + Definitions.END_LINE
+            + Definitions.INDENT + ItemHelper.BOOK_AUTHOR + BOOK_AUTHOR + Definitions.END_LINE
+            + Definitions.INDENT + ItemHelper.BOOK_DESCRIPTION + BOOK_DESCRIPTION + Definitions.END_LINE
+            + Definitions.INDENT + ItemHelper.BOOK_ISBN + BOOK_ISBN + Definitions.END_LINE;
+        Book book = new Book( BOOK_NAME, BOOK_DESCRIPTION, BOOK_AUTHOR, BOOK_ISBN );
+        book.accept( _visitor );
+        assertEquals( EXPECTED_STRING, _visitor.getResult() );
+    }
+
+    @Test
+    public void test_visiting_collection_setting_item_info_to_collection_info()
+    {
+        final String COLLECTION_NAME = "Data Structures and Algorithms Collection";
+        final String COLLECTION_DESCRIPTION = "This is a data structures and algorithms collection";
+        final String EXPECTED_STRING = ItemHelper.COLLECTION_NAME + COLLECTION_NAME + Definitions.END_LINE
+            + Definitions.INDENT + ItemHelper.COLLECTION_DESCRIPTION + COLLECTION_DESCRIPTION + Definitions.END_LINE;
+        Collection collection = new Collection( COLLECTION_NAME, COLLECTION_DESCRIPTION );
+        collection.accept( _visitor );
+        assertEquals( EXPECTED_STRING, _visitor.getResult() );
+    }
+
+    @Test
+    public void test_getting_result_returning_visitor_item_info()
+    {
+        assertSame( GetItemInfo(), _visitor.getResult() );
+    }
 }
