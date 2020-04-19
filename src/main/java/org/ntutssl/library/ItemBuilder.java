8865bd741ca173e6b7400a936e52524fc711a00c
kyo <phongbinh2511@gmail.com>
2020-04-19 08:56:39 +0800

    [feature-implement-homework-5] Added tests and implementation for building collection using `ItemBuilder`.

--- src/main/java/org/ntutssl/library/ItemBuilder.java
+++ src/main/java/org/ntutssl/library/ItemBuilder.java
@@ -1,17 +1,46 @@
 package org.ntutssl.library;
 
+import java.util.Stack;
+
 public class ItemBuilder
 {
     private Item _item;
+    private Stack< Collection > _collectionsStack;
 
     public ItemBuilder()
     {
-        /* Body intentionally empty */
+        _collectionsStack = new Stack< Collection >();
     }
 
     public void buildBook( String bookName, String bookDescription, String bookAuthor, String bookIsbn )
     {
-        _item = new Book( bookName, bookDescription, bookAuthor, bookIsbn );
+        Book book = new Book( bookName, bookDescription, bookAuthor, bookIsbn );
+        if ( _collectionsStack.empty() )
+        {
+            _item = book;
+        }
+        else
+        {
+            _collectionsStack.peek().add( book );
+        }
+    }
+
+    public void beginBuildingCollection( String collectionName, String collectionDescription )
+    {
+        _collectionsStack.push( new Collection( collectionName, collectionDescription ) );
+    }
+
+    public void endBuildingCollection()
+    {
+        Collection collection = _collectionsStack.pop();
+        if ( _collectionsStack.empty() )
+        {
+            _item = collection;
+        }
+        else
+        {
+            _collectionsStack.peek().add( collection );
+        }
     }
 
     public Item getResult()
--- src/test/java/org/ntutssl/library/ItemBuilderTest.java
+++ src/test/java/org/ntutssl/library/ItemBuilderTest.java
@@ -1,10 +1,12 @@
 package org.ntutssl.library;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
 import java.lang.reflect.Field;
+import java.util.Iterator;
 
 import org.junit.After;
 import org.junit.Before;
@@ -60,12 +62,31 @@ public class ItemBuilderTest
         throw new IllegalStateException( ITEM_CANNOT_BE_GOT );
     }
 
+    private void buildCollectionTwo()
+    {
+        _itemBuilder.beginBuildingCollection( COLLECTION_2_NAME, COLLECTION_2_DESCRIPTION );
+        _itemBuilder.buildBook( BOOK_1_NAME, BOOK_1_DESCRIPTION, BOOK_1_AUTHOR, BOOK_1_ISBN );
+        _itemBuilder.buildBook( BOOK_2_NAME, BOOK_2_DESCRIPTION, BOOK_2_AUTHOR, BOOK_2_ISBN );
+        _itemBuilder.endBuildingCollection();
+    }
+
     private void assertBookEquals( String expectedBookName, String expectedBookDescription, String expectedBookAuthor, String expectedBookIsbn, Book book )
     {
         assertEquals( expectedBookName,        book.name() );
         assertEquals( expectedBookDescription, book.description() );
         assertEquals( expectedBookAuthor,      book.author() );
         assertEquals( expectedBookIsbn,        book.isbn() );
+    }
+
+    private void assertEqualsCollectionTwo( Collection collection )
+    {
+        assertEquals( COLLECTION_2_NAME,        collection.name() );
+        assertEquals( COLLECTION_2_DESCRIPTION, collection.description() );
+        assertEquals( 2,                        collection.size() );
+        Iterator< Item > iterator = collection.iterator();
+        assertBookEquals( BOOK_1_NAME, BOOK_1_DESCRIPTION, BOOK_1_AUTHOR, BOOK_1_ISBN, ( Book )iterator.next() );
+        assertBookEquals( BOOK_2_NAME, BOOK_2_DESCRIPTION, BOOK_2_AUTHOR, BOOK_2_ISBN, ( Book )iterator.next() );
+        assertFalse( iterator.hasNext() );
     }
 
     @Before
@@ -85,6 +106,30 @@ public class ItemBuilderTest
     {
         _itemBuilder.buildBook( BOOK_1_NAME, BOOK_1_DESCRIPTION, BOOK_1_AUTHOR, BOOK_1_ISBN );
         assertBookEquals( BOOK_1_NAME, BOOK_1_DESCRIPTION, BOOK_1_AUTHOR, BOOK_1_ISBN, ( Book )GetItem() );
+    }
+
+    @Test
+    public void test_building_c2()
+    {
+        buildCollectionTwo();
+        assertEqualsCollectionTwo( ( Collection )GetItem() );
+    }
+
+    @Test
+    public void test_building_c1()
+    {
+        _itemBuilder.beginBuildingCollection( COLLECTION_1_NAME, COLLECTION_1_DESCRIPTION );
+        buildCollectionTwo();
+        _itemBuilder.buildBook( BOOK_3_NAME, BOOK_3_DESCRIPTION, BOOK_3_AUTHOR, BOOK_3_ISBN );
+        _itemBuilder.endBuildingCollection();
+        Collection collectionOne = ( Collection )GetItem();
+        assertEquals( COLLECTION_1_NAME,        collectionOne.name() );
+        assertEquals( COLLECTION_1_DESCRIPTION, collectionOne.description() );
+        assertEquals( 3,                        collectionOne.size() );
+        Iterator< Item > iterator = collectionOne.iterator();
+        assertEqualsCollectionTwo( ( Collection )iterator.next() );
+        assertBookEquals( BOOK_3_NAME, BOOK_3_DESCRIPTION, BOOK_3_AUTHOR, BOOK_3_ISBN, ( Book )iterator.next() );
+        assertFalse( iterator.hasNext() );
     }
 
     @Test
