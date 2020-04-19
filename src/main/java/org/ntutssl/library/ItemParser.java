64705bb4ccbbb34833d0cf725a972e253c3f0c4d
kyo <phongbinh2511@gmail.com>
2020-04-19 12:40:50 +0800

    [feature-implement-homework-5] Added test and implementation for parsing a book.

--- src/main/java/org/ntutssl/library/ItemParser.java
+++ src/main/java/org/ntutssl/library/ItemParser.java
@@ -1,7 +1,16 @@
 package org.ntutssl.library;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.NoSuchElementException;
+
+import com.google.gson.stream.JsonReader;
+
 public class ItemParser
 {
+    private final String ERROR_JSON_OBJECT_PROPERTY_NAME_IS_NOT = "The name of the property of the JSON object is not";
+    private final String ERROR_ITEM_TYPE_IS_INVALID = "The item type is invalid";
+    private JsonReader _jsonReader;
     private ItemBuilder _itemBuilder;
 
     public ItemParser()
@@ -9,7 +18,63 @@ public class ItemParser
         _itemBuilder = new ItemBuilder();
     }
 
-    public void parseItem( String itemInJsonFormat );
+    public void parseItem( String itemJsonObject )
+    {
+        _jsonReader = new JsonReader( new StringReader( itemJsonObject ) );
+        parseJsonObjectThenBuildItem();
+    }
+
+    private void parseJsonObjectThenBuildItem()
+    {
+        try
+        {
+            _jsonReader.beginObject();
+            parseJsonObjectPropertiesThenBuildItem();
+            _jsonReader.endObject();
+        }
+        catch ( IOException exception )
+        {
+            throw new IllegalStateException( exception );
+        }
+    }
+
+    private void parseJsonObjectPropertiesThenBuildItem()
+    {
+        String itemType = nextJsonObjectPropertyStringValue( Definitions.JSON_OBJECT_PROPERTY_NAME_TYPE );
+        if ( itemType.equals( Definitions.JSON_OBJECT_TYPE_PROPERTY_VALUE_BOOK ) )
+        {
+            parseJsonObjectPropertiesThenBuildBook();
+        }
+        else
+        {
+            throw new NoSuchElementException( ERROR_ITEM_TYPE_IS_INVALID );
+        }
+    }
+
+    private void parseJsonObjectPropertiesThenBuildBook()
+    {
+        String bookName        = nextJsonObjectPropertyStringValue( Definitions.JSON_OBJECT_PROPERTY_NAME_NAME );
+        String bookDescription = nextJsonObjectPropertyStringValue( Definitions.JSON_OBJECT_PROPERTY_NAME_DESCRIPTION );
+        String bookAuthor      = nextJsonObjectPropertyStringValue( Definitions.JSON_OBJECT_PROPERTY_NAME_AUTHOR );
+        String bookIsbn        = nextJsonObjectPropertyStringValue( Definitions.JSON_OBJECT_PROPERTY_NAME_ISBN );
+        _itemBuilder.buildBook( bookName, bookDescription, bookAuthor, bookIsbn );
+    }
+
+    private String nextJsonObjectPropertyStringValue( String jsonObjectPropertyName )
+    {
+        try
+        {
+            if ( !_jsonReader.nextName().equals( jsonObjectPropertyName ) )
+            {
+                throw new IllegalStateException( ERROR_JSON_OBJECT_PROPERTY_NAME_IS_NOT + Definitions.SPACE + jsonObjectPropertyName );
+            }
+            return _jsonReader.nextString();
+        }
+        catch ( IOException exception )
+        {
+            throw new IllegalStateException( exception );
+        }
+    }
 
     public Item getResult()
     {
--- src/test/java/org/ntutssl/library/ItemParserTest.java
+++ src/test/java/org/ntutssl/library/ItemParserTest.java
@@ -1,5 +1,6 @@
 package org.ntutssl.library;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
@@ -11,6 +12,36 @@ import org.junit.Test;
 
 public class ItemParserTest
 {
+    // c1
+    private final String COLLECTION_1_NAME = "Programming Collection";
+    private final String COLLECTION_1_DESCRIPTION = "This is a programming collection.";
+    // - c2
+    private final String COLLECTION_2_NAME = "Data Structures and Algorithms Collection";
+    private final String COLLECTION_2_DESCRIPTION = "This is a data structures and algorithms collection";
+    // -- b1
+    private final String BOOK_1_NAME = "Introduction to Algorithms, Third Edition";
+    private final String BOOK_1_DESCRIPTION = "The book covers a broad range of algorithms in depth, yet makes their design and analysis accessible to all levels of readers.";
+    private final String BOOK_1_AUTHOR = "Thomas H. Cormen";
+    private final String BOOK_1_ISBN = "9780262033848";
+    // -- b2
+    private final String BOOK_2_NAME = "Data Structures and Algorithms in Python";
+    private final String BOOK_2_DESCRIPTION = "Based on the authors' market leading data structures books in Java and C++, this book offers a comprehensive, definitive introduction to data structures in Python by authoritative authors.";
+    private final String BOOK_2_AUTHOR = "Michael H. Goldwasser";
+    private final String BOOK_2_ISBN = "9781118290279";
+    // - b3
+    private final String BOOK_3_NAME = "Design Patterns: Elements of Reusable Object-Oriented Software";
+    private final String BOOK_3_DESCRIPTION = "This is a software engineering book describing software design patterns.";
+    private final String BOOK_3_AUTHOR = "Gang of Four (GoF)";
+    private final String BOOK_3_ISBN = "9780201633610";
+    // b1 JSON object
+    private final String BOOK_1_JSON_OBJECT = Definitions.OPENING_CURLY_BRACE + Definitions.END_LINE
+        + Definitions.QUOTATION_MARK + Definitions.JSON_OBJECT_PROPERTY_NAME_TYPE        + Definitions.QUOTATION_MARK + Definitions.COLON + Definitions.SPACE + Definitions.QUOTATION_MARK + Definitions.JSON_OBJECT_TYPE_PROPERTY_VALUE_BOOK + Definitions.QUOTATION_MARK + Definitions.COMMA + Definitions.END_LINE
+        + Definitions.QUOTATION_MARK + Definitions.JSON_OBJECT_PROPERTY_NAME_NAME        + Definitions.QUOTATION_MARK + Definitions.COLON + Definitions.SPACE + Definitions.QUOTATION_MARK + BOOK_1_NAME                                      + Definitions.QUOTATION_MARK + Definitions.COMMA + Definitions.END_LINE
+        + Definitions.QUOTATION_MARK + Definitions.JSON_OBJECT_PROPERTY_NAME_DESCRIPTION + Definitions.QUOTATION_MARK + Definitions.COLON + Definitions.SPACE + Definitions.QUOTATION_MARK + BOOK_1_DESCRIPTION                               + Definitions.QUOTATION_MARK + Definitions.COMMA + Definitions.END_LINE
+        + Definitions.QUOTATION_MARK + Definitions.JSON_OBJECT_PROPERTY_NAME_AUTHOR      + Definitions.QUOTATION_MARK + Definitions.COLON + Definitions.SPACE + Definitions.QUOTATION_MARK + BOOK_1_AUTHOR                                    + Definitions.QUOTATION_MARK + Definitions.COMMA + Definitions.END_LINE
+        + Definitions.QUOTATION_MARK + Definitions.JSON_OBJECT_PROPERTY_NAME_ISBN        + Definitions.QUOTATION_MARK + Definitions.COLON + Definitions.SPACE + Definitions.QUOTATION_MARK + BOOK_1_ISBN                                      + Definitions.QUOTATION_MARK + Definitions.END_LINE
+        + Definitions.CLOSING_CURLY_BRACE;
+
     private final String ITEM_BUILDER_CANNOT_BE_GOT = "The member variable item builder cannot be got";
     private final String MEMBER_VARIABLE_NAME_ITEM_BUILDER = "_itemBuilder";
     private ItemParser _itemParser;
@@ -37,6 +68,14 @@ public class ItemParserTest
         throw new IllegalStateException( ITEM_BUILDER_CANNOT_BE_GOT );
     }
 
+    private void assertBookEquals( String expectedBookName, String expectedBookDescription, String expectedBookAuthor, String expectedBookIsbn, Book book )
+    {
+        assertEquals( expectedBookName,        book.name() );
+        assertEquals( expectedBookDescription, book.description() );
+        assertEquals( expectedBookAuthor,      book.author() );
+        assertEquals( expectedBookIsbn,        book.isbn() );
+    }
+
     @Before
     public void setUp()
     {
@@ -47,6 +86,13 @@ public class ItemParserTest
     public void tearDown()
     {
         /* Body intentionally empty */
+    }
+
+    @Test
+    public void test_parsing_b1()
+    {
+        _itemParser.parseItem( BOOK_1_JSON_OBJECT );
+        assertBookEquals( BOOK_1_NAME, BOOK_1_DESCRIPTION, BOOK_1_AUTHOR, BOOK_1_ISBN, ( Book )getItemBuilder().getResult() );
     }
 
     @Test
