3dd32eee3d0e8733020151aee3340d95dfa52a1e
kyo <phongbinh2511@gmail.com>
2020-04-20 19:33:01 +0800

    [feature-remove-get-json-object] Removed use of `ItemHelper.getJsonObject` in `WriteVisitor.visitBook`.

--- src/main/java/org/ntutssl/library/WriteVisitor.java
+++ src/main/java/org/ntutssl/library/WriteVisitor.java
@@ -17,7 +17,13 @@ public class WriteVisitor implements Visitor
         {
             _jsonArrayItems += Definitions.COMMA + Definitions.END_LINE;
         }
-        _jsonArrayItems += ItemHelper.getJsonObject( book, INITIAL_JSON_INDENTS_COUNT );
+        _jsonArrayItems += ItemHelper.getIndents( INITIAL_JSON_INDENTS_COUNT, Definitions.JSON_INDENT ) + Definitions.OPENING_CURLY_BRACE + Definitions.END_LINE
+                         + ItemHelper.getIndents( INITIAL_JSON_INDENTS_COUNT + 1, Definitions.JSON_INDENT ) + Definitions.QUOTATION_MARK + Definitions.JSON_OBJECT_PROPERTY_NAME_TYPE        + Definitions.QUOTATION_MARK + Definitions.COLON + Definitions.SPACE + Definitions.QUOTATION_MARK + Definitions.JSON_OBJECT_TYPE_PROPERTY_VALUE_BOOK + Definitions.QUOTATION_MARK + Definitions.COMMA + Definitions.END_LINE
+                         + ItemHelper.getIndents( INITIAL_JSON_INDENTS_COUNT + 1, Definitions.JSON_INDENT ) + Definitions.QUOTATION_MARK + Definitions.JSON_OBJECT_PROPERTY_NAME_NAME        + Definitions.QUOTATION_MARK + Definitions.COLON + Definitions.SPACE + Definitions.QUOTATION_MARK + book.name()                                      + Definitions.QUOTATION_MARK + Definitions.COMMA + Definitions.END_LINE
+                         + ItemHelper.getIndents( INITIAL_JSON_INDENTS_COUNT + 1, Definitions.JSON_INDENT ) + Definitions.QUOTATION_MARK + Definitions.JSON_OBJECT_PROPERTY_NAME_DESCRIPTION + Definitions.QUOTATION_MARK + Definitions.COLON + Definitions.SPACE + Definitions.QUOTATION_MARK + book.description()                               + Definitions.QUOTATION_MARK + Definitions.COMMA + Definitions.END_LINE
+                         + ItemHelper.getIndents( INITIAL_JSON_INDENTS_COUNT + 1, Definitions.JSON_INDENT ) + Definitions.QUOTATION_MARK + Definitions.JSON_OBJECT_PROPERTY_NAME_AUTHOR      + Definitions.QUOTATION_MARK + Definitions.COLON + Definitions.SPACE + Definitions.QUOTATION_MARK + book.author()                                    + Definitions.QUOTATION_MARK + Definitions.COMMA + Definitions.END_LINE
+                         + ItemHelper.getIndents( INITIAL_JSON_INDENTS_COUNT + 1, Definitions.JSON_INDENT ) + Definitions.QUOTATION_MARK + Definitions.JSON_OBJECT_PROPERTY_NAME_ISBN        + Definitions.QUOTATION_MARK + Definitions.COLON + Definitions.SPACE + Definitions.QUOTATION_MARK + book.isbn()                                      + Definitions.QUOTATION_MARK + Definitions.END_LINE
+                         + ItemHelper.getIndents( INITIAL_JSON_INDENTS_COUNT, Definitions.JSON_INDENT ) + Definitions.CLOSING_CURLY_BRACE;
     }
 
     @Override
