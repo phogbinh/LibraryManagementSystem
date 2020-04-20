b3d645a382bd25c71bd5cfc347356e45fdd7c451
kyo <phongbinh2511@gmail.com>
2020-04-20 19:52:00 +0800

    [feature-remove-get-json-object] Removed `ItemHelper.getJsonObject`.

--- src/main/java/org/ntutssl/library/ItemHelper.java
+++ src/main/java/org/ntutssl/library/ItemHelper.java
@@ -17,45 +17,6 @@ public final class ItemHelper
         /* Body intentionally empty */
     }
 
-    public static String getJsonObject( Item item, int initialJsonIndentsCount )
-    {
-        if ( item instanceof Book )
-        {
-            return getIndents( initialJsonIndentsCount, Definitions.JSON_INDENT ) + Definitions.OPENING_CURLY_BRACE + Definitions.END_LINE
-                 + getIndents( initialJsonIndentsCount + 1, Definitions.JSON_INDENT ) + Definitions.QUOTATION_MARK + Definitions.JSON_OBJECT_PROPERTY_NAME_TYPE        + Definitions.QUOTATION_MARK + Definitions.COLON + Definitions.SPACE + Definitions.QUOTATION_MARK + Definitions.JSON_OBJECT_TYPE_PROPERTY_VALUE_BOOK + Definitions.QUOTATION_MARK + Definitions.COMMA + Definitions.END_LINE
-                 + getIndents( initialJsonIndentsCount + 1, Definitions.JSON_INDENT ) + Definitions.QUOTATION_MARK + Definitions.JSON_OBJECT_PROPERTY_NAME_NAME        + Definitions.QUOTATION_MARK + Definitions.COLON + Definitions.SPACE + Definitions.QUOTATION_MARK + item.name()                                      + Definitions.QUOTATION_MARK + Definitions.COMMA + Definitions.END_LINE
-                 + getIndents( initialJsonIndentsCount + 1, Definitions.JSON_INDENT ) + Definitions.QUOTATION_MARK + Definitions.JSON_OBJECT_PROPERTY_NAME_DESCRIPTION + Definitions.QUOTATION_MARK + Definitions.COLON + Definitions.SPACE + Definitions.QUOTATION_MARK + item.description()                               + Definitions.QUOTATION_MARK + Definitions.COMMA + Definitions.END_LINE
-                 + getIndents( initialJsonIndentsCount + 1, Definitions.JSON_INDENT ) + Definitions.QUOTATION_MARK + Definitions.JSON_OBJECT_PROPERTY_NAME_AUTHOR      + Definitions.QUOTATION_MARK + Definitions.COLON + Definitions.SPACE + Definitions.QUOTATION_MARK + item.author()                                    + Definitions.QUOTATION_MARK + Definitions.COMMA + Definitions.END_LINE
-                 + getIndents( initialJsonIndentsCount + 1, Definitions.JSON_INDENT ) + Definitions.QUOTATION_MARK + Definitions.JSON_OBJECT_PROPERTY_NAME_ISBN        + Definitions.QUOTATION_MARK + Definitions.COLON + Definitions.SPACE + Definitions.QUOTATION_MARK + item.isbn()                                      + Definitions.QUOTATION_MARK + Definitions.END_LINE
-                 + getIndents( initialJsonIndentsCount, Definitions.JSON_INDENT ) + Definitions.CLOSING_CURLY_BRACE;
-        }
-        else if ( item instanceof Collection )
-        {
-            String collectionJsonObject = getIndents( initialJsonIndentsCount, Definitions.JSON_INDENT ) + Definitions.OPENING_CURLY_BRACE + Definitions.END_LINE
-                + getIndents( initialJsonIndentsCount + 1, Definitions.JSON_INDENT ) + Definitions.QUOTATION_MARK + Definitions.JSON_OBJECT_PROPERTY_NAME_TYPE        + Definitions.QUOTATION_MARK + Definitions.COLON + Definitions.SPACE + Definitions.QUOTATION_MARK + Definitions.JSON_OBJECT_TYPE_PROPERTY_VALUE_COLLECTION + Definitions.QUOTATION_MARK + Definitions.COMMA + Definitions.END_LINE
-                + getIndents( initialJsonIndentsCount + 1, Definitions.JSON_INDENT ) + Definitions.QUOTATION_MARK + Definitions.JSON_OBJECT_PROPERTY_NAME_NAME        + Definitions.QUOTATION_MARK + Definitions.COLON + Definitions.SPACE + Definitions.QUOTATION_MARK + item.name()                                            + Definitions.QUOTATION_MARK + Definitions.COMMA + Definitions.END_LINE
-                + getIndents( initialJsonIndentsCount + 1, Definitions.JSON_INDENT ) + Definitions.QUOTATION_MARK + Definitions.JSON_OBJECT_PROPERTY_NAME_DESCRIPTION + Definitions.QUOTATION_MARK + Definitions.COLON + Definitions.SPACE + Definitions.QUOTATION_MARK + item.description()                                     + Definitions.QUOTATION_MARK + Definitions.COMMA + Definitions.END_LINE
-                + getIndents( initialJsonIndentsCount + 1, Definitions.JSON_INDENT ) + Definitions.QUOTATION_MARK + Definitions.JSON_OBJECT_PROPERTY_NAME_ITEMS       + Definitions.QUOTATION_MARK + Definitions.COLON + Definitions.SPACE + Definitions.OPENING_SQUARE_BRACKET + Definitions.END_LINE;
-            Iterator< Item > iterator = item.iterator();
-            while ( iterator.hasNext() )
-            {
-                collectionJsonObject += getJsonObject( iterator.next(), initialJsonIndentsCount + 2 );
-                if ( iterator.hasNext() )
-                {
-                    collectionJsonObject += Definitions.COMMA;
-                }
-                collectionJsonObject += Definitions.END_LINE;
-            }
-            collectionJsonObject += getIndents( initialJsonIndentsCount + 1, Definitions.JSON_INDENT ) + Definitions.CLOSING_SQUARE_BRACKET + Definitions.END_LINE
-                + getIndents( initialJsonIndentsCount, Definitions.JSON_INDENT ) + Definitions.CLOSING_CURLY_BRACE;
-            return collectionJsonObject;
-        }
-        else
-        {
-            throw new IllegalStateException( ERROR_ITEM_IS_OF_INVALID_TYPE );
-        }
-    }
-
     public static String getIndents( int indentsCount, String indent )
     {
         String indents = "";
