f8e700f67958f84f8a31f3caa304d10b689ae217
kyo <phongbinh2511@gmail.com>
2020-04-20 20:17:19 +0800

    [feature-remove-constants] Removed the error message of `ItemHelper`.

--- src/main/java/org/ntutssl/library/ItemHelper.java
+++ src/main/java/org/ntutssl/library/ItemHelper.java
@@ -4,8 +4,6 @@ import java.util.Iterator;
 
 public final class ItemHelper
 {
-    public static String ERROR_ITEM_IS_OF_INVALID_TYPE = "The given item is of invalid type";
-
     private ItemHelper()
     {
         /* Body intentionally empty */
