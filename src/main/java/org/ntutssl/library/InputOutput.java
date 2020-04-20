c8fb67adde4c7259149dddf25cab08d14d6e86f8
kyo <phongbinh2511@gmail.com>
2020-04-20 18:42:11 +0800

    [feature-update-input-output] Formatted declarations of constants.

--- src/main/java/org/ntutssl/library/InputOutput.java
+++ src/main/java/org/ntutssl/library/InputOutput.java
@@ -5,45 +5,45 @@ import java.util.Iterator;
 
 public class InputOutput
 {
-    private final String COMMAND_LIBRARY_ADD_BOOK = "1";
+    private final String COMMAND_LIBRARY_ADD_BOOK       = "1";
     private final String COMMAND_LIBRARY_ADD_COLLECTION = "2";
-    private final String COMMAND_LIBRARY_IMPORT = "3";
-    private final String COMMAND_LIBRARY_EXPORT = "4";
-    private final String COMMAND_LIBRARY_LIST_ALL = "5";
-    private final String COMMAND_LIBRARY_FIND = "6";
-    private final String COMMAND_LIBRARY_EXIT = "7";
+    private final String COMMAND_LIBRARY_IMPORT         = "3";
+    private final String COMMAND_LIBRARY_EXPORT         = "4";
+    private final String COMMAND_LIBRARY_LIST_ALL       = "5";
+    private final String COMMAND_LIBRARY_FIND           = "6";
+    private final String COMMAND_LIBRARY_EXIT           = "7";
 
-    private final String COMMAND_COLLECTION_ADD_BOOK = "1";
+    private final String COMMAND_COLLECTION_ADD_BOOK       = "1";
     private final String COMMAND_COLLECTION_ADD_COLLECTION = "2";
-    private final String COMMAND_COLLECTION_EXIT = "3";
+    private final String COMMAND_COLLECTION_EXIT           = "3";
 
-    private final String INSTRUCTION_ADD_BOOK = "Please enter the informations of books" + Definitions.COLON;
+    private final String INSTRUCTION_ADD_BOOK       = "Please enter the informations of books"     + Definitions.COLON;
     private final String INSTRUCTION_ADD_COLLECTION = "Please enter the infomations of collection" + Definitions.COLON;
     private final String INSTRUCTION_INPUT_LIBRARY_COMMAND = "Please enter the instruction as following to manage the library:" + Definitions.END_LINE
-        + Definitions.INDENT + "1. 'Add book': to add book to the library" + Definitions.END_LINE
-        + Definitions.INDENT + "2. 'Add collection': to add a collection to the library" + Definitions.END_LINE
-        + Definitions.INDENT + "3. 'import': import the json file to library." + Definitions.END_LINE
-        + Definitions.INDENT + "4. 'export': export the library to json file." + Definitions.END_LINE
+        + Definitions.INDENT + "1. 'Add book': to add book to the library"                         + Definitions.END_LINE
+        + Definitions.INDENT + "2. 'Add collection': to add a collection to the library"           + Definitions.END_LINE
+        + Definitions.INDENT + "3. 'import': import the json file to library."                     + Definitions.END_LINE
+        + Definitions.INDENT + "4. 'export': export the library to json file."                     + Definitions.END_LINE
         + Definitions.INDENT + "5. 'list all': to list the detail of all the items in the library" + Definitions.END_LINE
-        + Definitions.INDENT + "6. 'find': to find the item(s) in the library." + Definitions.END_LINE
+        + Definitions.INDENT + "6. 'find': to find the item(s) in the library."                    + Definitions.END_LINE
         + Definitions.INDENT + "7. 'exit': to exit the program.";
     private final String INSTRUCTION_INPUT_COLLECTION_COMMAND = "Please enter the instruction as following to manage the collection:" + Definitions.END_LINE
-        + Definitions.INDENT + "1. 'Add book': to add book to the collection" + Definitions.END_LINE
+        + Definitions.INDENT + "1. 'Add book': to add book to the collection"               + Definitions.END_LINE
         + Definitions.INDENT + "2. 'Add collection': to add a collection to the collection" + Definitions.END_LINE
         + Definitions.INDENT + "3. 'exit': to exit the process.";
-    private final String INSTRUCTION_INPUT_BOOK_NAME = "Name of book" + Definitions.COLON + Definitions.SPACE;
-    private final String INSTRUCTION_INPUT_BOOK_DESCRIPTION = "Description of book" + Definitions.COLON + Definitions.SPACE;
-    private final String INSTRUCTION_INPUT_BOOK_AUTHOR = "Author of book" + Definitions.COLON + Definitions.SPACE;
-    private final String INSTRUCTION_INPUT_BOOK_ISBN = "ISBN of book" + Definitions.COLON + Definitions.SPACE;
-    private final String INSTRUCTION_INPUT_COLLECTION_NAME = "Name of collection" + Definitions.COLON + Definitions.SPACE;
-    private final String INSTRUCTION_INPUT_COLLECTION_DESCRIPTION = "Description of collection" + Definitions.COLON + Definitions.SPACE;
-    private final String INSTRUCTION_INPUT_TO_BE_FOUND_ITEM_NAME = "Enter the name of the item to find" + Definitions.COLON + Definitions.SPACE;
+    private final String INSTRUCTION_INPUT_BOOK_NAME                     = "Name of book"                                       + Definitions.COLON + Definitions.SPACE;
+    private final String INSTRUCTION_INPUT_BOOK_DESCRIPTION              = "Description of book"                                + Definitions.COLON + Definitions.SPACE;
+    private final String INSTRUCTION_INPUT_BOOK_AUTHOR                   = "Author of book"                                     + Definitions.COLON + Definitions.SPACE;
+    private final String INSTRUCTION_INPUT_BOOK_ISBN                     = "ISBN of book"                                       + Definitions.COLON + Definitions.SPACE;
+    private final String INSTRUCTION_INPUT_COLLECTION_NAME               = "Name of collection"                                 + Definitions.COLON + Definitions.SPACE;
+    private final String INSTRUCTION_INPUT_COLLECTION_DESCRIPTION        = "Description of collection"                          + Definitions.COLON + Definitions.SPACE;
+    private final String INSTRUCTION_INPUT_TO_BE_FOUND_ITEM_NAME         = "Enter the name of the item to find"                 + Definitions.COLON + Definitions.SPACE;
     private final String INSTRUCTION_INPUT_TO_BE_IMPORTED_JSON_FILE_PATH = "Please enter the json file path you want to import" + Definitions.COLON + Definitions.SPACE;
     private final String INSTRUCTION_INPUT_TO_BE_EXPORTED_JSON_FILE_PATH = "Please enter the json file path you want to export" + Definitions.COLON + Definitions.SPACE;
 
-    private final String BOOK = "Book";
+    private final String BOOK       = "Book";
     private final String COLLECTION = "Collection";
-    private final String ADDED = "added";
+    private final String ADDED      = "added";
 
     private final String ERROR_COMMAND_IS_INVALID = "The given command is invalid";
 
