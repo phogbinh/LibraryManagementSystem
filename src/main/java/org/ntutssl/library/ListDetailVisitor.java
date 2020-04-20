be4eac16770c114bf07fb02c3b5efda6e947f5ef
kyo <phongbinh2511@gmail.com>
2020-04-20 20:12:12 +0800

    [feature-remove-constants] Removed use of 6 constants of `ItemHelper` in `ListDetailVisitor`.

--- src/main/java/org/ntutssl/library/ListDetailVisitor.java
+++ src/main/java/org/ntutssl/library/ListDetailVisitor.java
@@ -16,17 +16,17 @@ public class ListDetailVisitor implements Visitor
     @Override
     public void visitBook( Book book )
     {
-        _itemInfo += Definitions.getIndents( _level, Definitions.INDENT )     + ItemHelper.BOOK_NAME        + book.name()        + Definitions.END_LINE
-                   + Definitions.getIndents( _level + 1, Definitions.INDENT ) + ItemHelper.BOOK_AUTHOR      + book.author()      + Definitions.END_LINE
-                   + Definitions.getIndents( _level + 1, Definitions.INDENT ) + ItemHelper.BOOK_DESCRIPTION + book.description() + Definitions.END_LINE
-                   + Definitions.getIndents( _level + 1, Definitions.INDENT ) + ItemHelper.BOOK_ISBN        + book.isbn()        + Definitions.END_LINE;
+        _itemInfo += Definitions.getIndents( _level, Definitions.INDENT )     + Definitions.BOOK_NAME        + book.name()        + Definitions.END_LINE
+                   + Definitions.getIndents( _level + 1, Definitions.INDENT ) + Definitions.BOOK_AUTHOR      + book.author()      + Definitions.END_LINE
+                   + Definitions.getIndents( _level + 1, Definitions.INDENT ) + Definitions.BOOK_DESCRIPTION + book.description() + Definitions.END_LINE
+                   + Definitions.getIndents( _level + 1, Definitions.INDENT ) + Definitions.BOOK_ISBN        + book.isbn()        + Definitions.END_LINE;
     }
 
     @Override
     public void visitCollection( Collection collection )
     {
-        _itemInfo += Definitions.getIndents( _level, Definitions.INDENT )     + ItemHelper.COLLECTION_NAME        + collection.name()        + Definitions.END_LINE
-                   + Definitions.getIndents( _level + 1, Definitions.INDENT ) + ItemHelper.COLLECTION_DESCRIPTION + collection.description() + Definitions.END_LINE;
+        _itemInfo += Definitions.getIndents( _level, Definitions.INDENT )     + Definitions.COLLECTION_NAME        + collection.name()        + Definitions.END_LINE
+                   + Definitions.getIndents( _level + 1, Definitions.INDENT ) + Definitions.COLLECTION_DESCRIPTION + collection.description() + Definitions.END_LINE;
         Iterator< Item > iterator = collection.iterator();
         _level++;
         while ( iterator.hasNext() )
